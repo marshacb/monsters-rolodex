a14c45b26a7245c4153a6a786d9a97fb42d02b6b
Cameron <marshacb@Camerons-MBP.fios-router.home>
2020-01-27 13:51:14 -0500

    Updates

---
+++ precache-manifest.538099ca1916acc5d5248f002cc68760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266d0f0c460efe681b1110b3b4fa0ada",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c5a58c5035348dbd97a6",
+    "url": "/monsters-rolodex/static/css/main.646c2f3f.chunk.css"
+  },
+  {
+    "revision": "105422028b6763bd0844",
+    "url": "/monsters-rolodex/static/js/2.f35d3ce1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.f35d3ce1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5a58c5035348dbd97a6",
+    "url": "/monsters-rolodex/static/js/main.4c9f9898.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
